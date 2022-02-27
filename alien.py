759654a62c94ffcd2cc525921772e433875a7237
BlvckJack <ogonofidel@gmail.com>
2022-02-27 17:10:53 +0300

    Modified alien file.

--- alien.py
+++ alien.py
@@ -1,3 +1,4 @@
+
 aliens = [] 
 
 
@@ -10,7 +11,46 @@ for alien_num in range(30):
     }
     aliens.append(new_alien)
 
-for alien in aliens[:5]:
-    print(alien)
+for alien in aliens[10:20]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+    
 
-print("\t\n Total number of aliens : {}".format(len(aliens)))
+for alien in aliens[20:30]:
+    if alien['color'] == 'green':
+        alien['color'] = 'red'
+        alien['points'] = 15
+        alien['speed'] = 'fast'
+
+
+def alien_total():
+    print("\n\nNumber of green aliens : {}".format(len([alien for alien in aliens[0:10] if isinstance(alien, dict)])))
+    print("Number of yellow aliens : {}".format(len([alien for alien in aliens[10:20] if isinstance(alien, dict)])))
+    print("Number of red aliens : {}".format(len([alien for alien in aliens[20:30] if isinstance(alien, dict)])))
+
+
+for alien in aliens:
+    alien['x_coordinate'] = 0
+    alien['y_coordinate'] = 15
+
+
+def speed(a):
+        if a == 'green':
+            x_increment = 3
+            y_increment = 6
+
+        elif a == 'yellow':
+            x_increment = 5
+            y_increment = 10
+
+        else:
+            x_increment = 7
+            y_increment = 14
+
+        print("\n\nNew " + str(a) + " alien coordinate is : {}\n\n".format((alien['x_coordinate'] + x_increment, alien['y_coordinate'] + y_increment)))
+
+
+alien_total()
+speed('green')
